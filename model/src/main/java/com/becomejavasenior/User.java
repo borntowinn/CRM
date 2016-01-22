7074765197f9fab1275263ea78ed506b79c9c6bb
Default71721 <lavaki@gmail.com>
2016-01-22 11:26:10 +0200

    user bean (test commit)

---
+++ model/src/main/java/com/becomejavasenior/User.java
@@ -0,0 +1,95 @@
+package com.becomejavasenior;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Default71721 on 22.01.16.
+ */
+public class User implements Serializable {
+    //irrelevant comment
+    private Integer id;
+    private String name;
+    private String surname;
+    private String password;
+    private Date creationDate;
+    private Integer userRoleID;
+    private String email;
+    private String mobilePhone;
+    private String workPhone;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getUserRoleID() {
+        return userRoleID;
+    }
+
+    public void setUserRoleID(Integer userRoleID) {
+        this.userRoleID = userRoleID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    public void setWorkPhone(String workPhone) {
+        this.workPhone = workPhone;
+    }
+}
