08e517074b8135310b430e9df31f80d676712643
Default71721 <lavaki@gmail.com>
2016-02-15 11:59:21 +0200

    CRMATL-166 - DB refactoring(comment and file tables) and related DAO changes

--- jdbc/src/main/java/com/becomejavasenior/dao/jdbc/impl/CommentDaoImpl.java
+++ jdbc/src/main/java/com/becomejavasenior/dao/jdbc/impl/CommentDaoImpl.java
@@ -1,8 +1,9 @@
 package com.becomejavasenior.dao.jdbc.impl;
 
-import com.becomejavasenior.Comment;
-import com.becomejavasenior.dao.CommentDao;
+import com.becomejavasenior.*;
+import com.becomejavasenior.dao.*;
 import com.becomejavasenior.dao.exception.PersistException;
+import com.becomejavasenior.dao.jdbc.factory.DaoFactory;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,10 +16,10 @@ import java.util.List;
  * Created by Default71721 on 10.02.16.
  */
 public class CommentDaoImpl extends AbstractJDBCDao<Comment> implements CommentDao<Comment> {
-    private final static String SELECT_QUERY = "SELECT comment_id, comment, data_creation FROM comment;";
-    private final static String SELECT_BY_PK_QUERY = "SELECT comment_id, comment, data_creation FROM comment WHERE comment_id = ?;";
-    private final static String CREATE_QUERY = "INSERT INTO comment (comment, data_creation) VALUES (?, ?);";
-    private final static String UPDATE_QUERY = "UPDATE comment SET comment = ?, data_creation = ?  WHERE comment_id = ?;";
+    private final static String SELECT_QUERY = "SELECT comment_id, comment, data_creation, file_name, company_id, contact_id, deal_id, task_id FROM comment;";
+    private final static String SELECT_BY_PK_QUERY = "SELECT comment_id, comment, data_creation, company_id, contact_id, deal_id, task_id FROM comment WHERE comment_id = ?;";
+    private final static String CREATE_QUERY = "INSERT INTO comment (comment, data_creation, company_id, contact_id, deal_id, task_id) VALUES (?, ?);";
+    private final static String UPDATE_QUERY = "UPDATE comment SET comment = ?, data_creation = ?, company_id = ?, contact_id = ?, deal_id = ?, task_id = ? WHERE comment_id = ?;";
     private final static String DELETE_QUERY = "DELETE FROM comment WHERE comment_id= ?;";
 
     @Override
@@ -54,12 +55,20 @@ public class CommentDaoImpl extends AbstractJDBCDao<Comment> implements CommentD
     @Override
     protected List<Comment> parseResultSet(ResultSet rs) throws PersistException {
         ArrayList<Comment> result = new ArrayList<>();
+        CompanyDao<Company> companyDao = DaoFactory.getCompanyDAO();
+        ContactDao<Contact> contactDao = DaoFactory.getContactDao();
+        DealDao<Deal> dealDao = DaoFactory.getDealDao();
+        TaskDao<Task> taskDao = DaoFactory.getTaskDao();
         try {
             while (rs.next()) {
                 Comment comment = new Comment();
                 comment.setId(rs.getInt("comment_id"));
                 comment.setComment(rs.getString("comment"));
                 comment.setCreationDate(rs.getTimestamp("data_creation").toLocalDateTime());
+                comment.setCompanyId(companyDao.getByPK(rs.getInt("company_id")));
+                comment.setContactId(contactDao.getByPK(rs.getInt("contact_id")));
+                comment.setDealId(dealDao.getByPK(rs.getInt("deal_id")));
+                comment.setTaskId(taskDao.getByPK(rs.getInt("task_id")));
                 result.add(comment);
             }
         } catch (SQLException e) {
@@ -73,6 +82,10 @@ public class CommentDaoImpl extends AbstractJDBCDao<Comment> implements CommentD
         try {
             statement.setString(1, comment.getComment());
             statement.setTimestamp(2, Timestamp.valueOf(comment.getCreationDate()));
+            statement.setInt(3, comment.getCompanyId().getId());
+            statement.setInt(4, comment.getContactId().getId());
+            statement.setInt(5, comment.getDealId().getId());
+            statement.setInt(6, comment.getTaskId().getId());
         } catch (SQLException e) {
             throw new PersistException(e);
         }
@@ -83,7 +96,11 @@ public class CommentDaoImpl extends AbstractJDBCDao<Comment> implements CommentD
         try {
             statement.setString(1, comment.getComment());
             statement.setTimestamp(2, Timestamp.valueOf(comment.getCreationDate()));
-            statement.setInt(3, comment.getId());
+            statement.setInt(3, comment.getCompanyId().getId());
+            statement.setInt(4, comment.getContactId().getId());
+            statement.setInt(5, comment.getDealId().getId());
+            statement.setInt(6, comment.getTaskId().getId());
+            statement.setInt(7, comment.getId());
         } catch (SQLException e) {
             throw new PersistException(e);
         }
--- jdbc/src/main/java/com/becomejavasenior/dao/jdbc/impl/FileDaoImpl.java
+++ jdbc/src/main/java/com/becomejavasenior/dao/jdbc/impl/FileDaoImpl.java
@@ -1,10 +1,9 @@
 package com.becomejavasenior.dao.jdbc.impl;
 
-import com.becomejavasenior.File;
-import com.becomejavasenior.UserRole;
-import com.becomejavasenior.dao.AbstractDao;
-import com.becomejavasenior.dao.FileDao;
+import com.becomejavasenior.*;
+import com.becomejavasenior.dao.*;
 import com.becomejavasenior.dao.exception.PersistException;
+import com.becomejavasenior.dao.jdbc.factory.DaoFactory;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,10 +17,10 @@ import java.util.List;
  * Created by Default71721 on 10.02.16.
  */
 public class FileDaoImpl extends AbstractJDBCDao<File> implements FileDao<File> {
-    private final static String SELECT_QUERY = "SELECT file_id, date_creation, file, file_name FROM file";
-    private final static String SELECT_BY_PK_QUERY = "SELECT file_id, date_creation, file, file_name FROM file WHERE file_id= ?";
-    private final static String CREATE_QUERY = "INSERT INTO file (date_creation, file, file_name) VALUES (?, ?, ?);";
-    private final static String UPDATE_QUERY = "UPDATE file SET date_creation= ?, file=?, file_name = ?  WHERE file_id= ?;";
+    private final static String SELECT_QUERY = "SELECT file_id, date_creation, file, file_name, company_id, contact_id, deal_id, user_id FROM file";
+    private final static String SELECT_BY_PK_QUERY = "SELECT file_id, date_creation, file, file_name, company_id, contact_id, deal_id, user_id FROM file WHERE file_id= ?";
+    private final static String CREATE_QUERY = "INSERT INTO file (date_creation, file, file_name, company_id, contact_id, deal_id, user_id) VALUES (?, ?, ?, ?, ?, ?, ?);";
+    private final static String UPDATE_QUERY = "UPDATE file SET date_creation= ?, file=?, file_name = ?, company_id = ?, contact_id = ?, deal_id = ?, user_id = ? WHERE file_id= ?;";
     private final static String DELETE_QUERY = "DELETE FROM file WHERE file_id= ?;";
 
     @Override
@@ -57,6 +56,11 @@ public class FileDaoImpl extends AbstractJDBCDao<File> implements FileDao<File> 
     @Override
     protected List<File> parseResultSet(ResultSet rs) throws PersistException {
         ArrayList<File> result = new ArrayList<>();
+        CompanyDao<Company> companyDao = DaoFactory.getCompanyDAO();
+        ContactDao<Contact> contactDao = DaoFactory.getContactDao();
+        DealDao<Deal> dealDao = DaoFactory.getDealDao();
+        UserDao<User> userDao = DaoFactory.getUserDAO();
+
         try {
             while (rs.next()) {
                 File file = new File();
@@ -64,6 +68,10 @@ public class FileDaoImpl extends AbstractJDBCDao<File> implements FileDao<File> 
                 file.setCreationDate(rs.getTimestamp("date_creation").toLocalDateTime());
                 file.setFile(rs.getBytes("file"));
                 file.setFileName(rs.getString("file_name"));
+                file.setCompanyId(companyDao.getByPK(rs.getInt("company_id")));
+                file.setContactId(contactDao.getByPK(rs.getInt("contact_id")));
+                file.setDealId(dealDao.getByPK(rs.getInt("deal_id")));
+                file.setUserId(userDao.getByPK(rs.getInt("user_id")));
                 result.add(file);
             }
         } catch (SQLException e) {
@@ -78,6 +86,10 @@ public class FileDaoImpl extends AbstractJDBCDao<File> implements FileDao<File> 
             statement.setTimestamp(1, Timestamp.valueOf(file.getCreationDate()));
             statement.setBytes(2, file.getFile());
             statement.setString(3, file.getFileName());
+            statement.setInt(4, file.getCompanyId().getId());
+            statement.setInt(5, file.getContactId().getId());
+            statement.setInt(6, file.getDealId().getId());
+            statement.setInt(7, file.getUserId().getId());
         } catch (SQLException e) {
             throw new PersistException(e);
         }
@@ -89,7 +101,11 @@ public class FileDaoImpl extends AbstractJDBCDao<File> implements FileDao<File> 
             statement.setTimestamp(1, Timestamp.valueOf(file.getCreationDate()));
             statement.setBytes(2, file.getFile());
             statement.setString(3, file.getFileName());
-            statement.setInt(4, file.getId());
+            statement.setInt(4, file.getCompanyId().getId());
+            statement.setInt(5, file.getContactId().getId());
+            statement.setInt(6, file.getDealId().getId());
+            statement.setInt(7, file.getUserId().getId());
+            statement.setInt(8, file.getId());
         } catch (SQLException e) {
             throw new PersistException(e);
         }
