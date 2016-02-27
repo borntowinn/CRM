b1f79f96d9ece8ecfc4dc439e820eb71a0ea0950
Ivan Vynogradov <vaniavinogradov@rambler.ru>
2016-02-27 16:43:13 +0200

    CRMATL-13 - changed ContactDaoImpl: inplemented addComment and addFile according to new database schema; Chanaged script for population database;

--- dao/src/main/java/resources/sql/crmAtllas_patch4.0-addedSeqForFileToContact.sql
+++
@@ -1,8 +0,0 @@
-CREATE SEQUENCE files_to_contact_files_to_contact_id_seq
-INCREMENT 1
-MINVALUE 1
-MAXVALUE 9223372036854775807
-START 1
-CACHE 1;
-ALTER TABLE files_to_contact ALTER COLUMN files_to_contact_id SET DEFAULT nextval('files_to_contact_files_to_contact_id_seq'::regclass);
-
